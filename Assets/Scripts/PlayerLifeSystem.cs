bcfd33997c93ee2b033794096bcc149191422da6
Brandan_Hummell <Brandan.Hummell@gmail.com>
2018-05-06 20:34:09 -0400

    Minor bug fix for the extra life text

--- Assets/Scripts/PlayerLifeSystem.cs
+++ Assets/Scripts/PlayerLifeSystem.cs
@@ -18,6 +18,6 @@ public class PlayerLifeSystem : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		lifeText.text = "Extra Lives: " + playerLives - 1;
+		lifeText.text = "Extra Lives: " + (playerLives - 1);
 	}
 }
