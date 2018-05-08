a1b825b393b5f42b5c427cd4844ffa8187975d22
Brandan_Hummell <Brandan.Hummell@gmail.com>
2018-05-07 22:36:27 -0400

    Changed the UI elements to scale on screen screen (looking primarily at height)

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -33,7 +33,7 @@ public class EnemyController : MonoBehaviour {
 				Instantiate (shot, enemy.position, enemy.rotation);
 			}
 
-			if (enemy.position.y <= -2) {
+			if (enemy.position.y <= -2.5) {
 				PlayerLifeSystem.playerLives = 0;
 				GameOver.isPlayerDead = true;
 				Time.timeScale = 0;
